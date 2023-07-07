aaf8a65a850e487325cec5be6fae9c533a02a039
Lalit Patil <90670414+lalit-patil005@users.noreply.github.com>
2023-07-07 05:40:29 +0000

    simple  web server WIP

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home Page Endpoint.")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
 
 func main() {
-	fmt.Println("Hello API")
+	handleRequests()
 }
