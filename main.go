a66775ec2731af13a6346a28f10570329a9ee607
Lalit Patil <90670414+lalit-patil005@users.noreply.github.com>
2023-07-07 06:06:41 +0000

    Test another endpoint

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test Title", Desc: "Test description", Content: " Test Content"},
+	}
+	fmt.Println("allArticles API hit.")
+	json.NewEncoder(w).Encode(articles)
+
+}
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Home Page Endpoint.")
 }
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
 
